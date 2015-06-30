ee0a392178c2dc350b8ec53285204b0f6a7e539a
Ian Schiffer <ischiffer@gmail.com>
2015-06-29 23:51:02 -0700

    Fix ajax tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -96,17 +96,17 @@
 
 			describe "in the Microposts controller" do
 
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
 
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-      end
-      
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+			
 			describe "as a wrong user" do
 				let(:user) { FactoryGirl.create(:user) }
 				let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
@@ -122,28 +122,28 @@
 					specify { expect(response).to redirect_to(root_url) }
 				end
 			end
-			    describe "as non-admin user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:non_admin) { FactoryGirl.create(:user) }
+			describe "as non-admin user" do
+				let(:user) { FactoryGirl.create(:user) }
+				let(:non_admin) { FactoryGirl.create(:user) }
 
-      before { sign_in non_admin, no_capybara: true }
+				before { sign_in non_admin, no_capybara: true }
 
-      describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { expect(response).to redirect_to(root_url) }
-      end
-    end
+				describe "submitting a DELETE request to the Users#destroy action" do
+					before { delete user_path(user) }
+					specify { expect(response).to redirect_to(root_url) }
+				end
+			end
 
-    describe "in the Relationships controller" do
-    	describe "submitting to the create action" do 
-    		before { post relationships_path }
-    		specify { expect(response).to redirect_to(signin_path) }
-    	end
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do 
+					before { post relationships_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
 
-    	describe "submitting to the create action" do
-    		before { delete relationships_path(1) }
-    		specify { expect(response).to redirect_to(signin_path) }
-    	end
-    end
-  end
-end
+				describe "submitting to the create action" do
+					before { delete relationship_path(1) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+		end
+	end
